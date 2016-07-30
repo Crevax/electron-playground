8a9f4b457ef0d0c5baf801735fdf219e5fd5e920
CJ Davis <cj@cjdavis.me>
2016-07-30 18:15:51 -0400

    Use gulp to create minified app script (downgrading to ES5 as well)

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+    gulp_concat = require('gulp-concat'),
+    gulp_uglify = require('gulp-uglify');
+
+
+gulp.task('build:js', function() {
+    return gulp.src(['src/js/**/*.js',])
+        .pipe(gulp_concat('app.min.js'))
+        .pipe(gulp_uglify())
+        .pipe(gulp.dest('src/dist/js/'));
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,3 @@
-window.onload = () => {
+window.onload =  function() {
     console.log('Hello world!');
 }
