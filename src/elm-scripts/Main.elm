d07f3dff063b3a2c66460e214164ee728b7a3472
CJ Davis <cj@cjdavis.me>
2016-08-26 19:29:16 -0400

    Update app to accept version flags for display

--- src/elm-scripts/Main.elm
+++ src/elm-scripts/Main.elm
@@ -1,18 +1,66 @@
 module Hello exposing (..)
 
 import Html exposing (Html, div, h1, text)
+import Html.App as App
 
 
-main : Html a
+main : Program Flags
 main =
-    view
+    App.programWithFlags
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
 
 
-view : Html a
-view =
+type alias Flags =
+    { chrome : String
+    , node : String
+    , electron : String
+    }
+
+
+type alias Model =
+    { versions :
+        { chrome : String
+        , node : String
+        , electron : String
+        }
+    }
+
+
+type Msg
+    = GetVersions
+
+
+init : Flags -> ( Model, Cmd msg )
+init flags =
+    ( { versions =
+            { chrome = toString flags.chrome
+            , node = toString flags.node
+            , electron = toString flags.electron
+            }
+      }
+    , Cmd.none
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GetVersions ->
+            ( { model | versions = model.versions }, Cmd.none )
+
+
+view : Model -> Html a
+view model =
     div []
         [ h1 [] [ text "Hello World!" ]
-        , text "We are using node x, "
-        , text "Chrome y, "
-        , text "and Electron z."
+        , text "We are using node "
+        , text (model.versions.node)
+        , text ", Chrome "
+        , text (model.versions.chrome)
+        , text ", and Electron "
+        , text (model.versions.electron)
         ]
